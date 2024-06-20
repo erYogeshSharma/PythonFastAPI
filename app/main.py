469bea2eb815ad77fc2b4392481e90361af50be9
= <=>
2024-06-20 18:14:06 +0530

    Refactor main.py to use PostgreSQL database for storing and retrieving posts

--- app/main.py
+++ app/main.py
@@ -1,13 +1,28 @@
-
-import re
-from turtle import pos
+import string
+import time
 from typing import Optional
 from fastapi import FastAPI, Response, status, HTTPException
 from fastapi.params import Body
+from httpx import delete
 from pydantic import BaseModel
 from random import randrange
+import psycopg2
+# This is used to return the data in the form of dictionary
+from psycopg2.extras import RealDictCursor
 
 app = FastAPI()
+
+while True:
+    try:
+        conn = psycopg2.connect(host="localhost", database="fastapi",
+                                user="postgres", password='yogesh084', cursor_factory=RealDictCursor)
+        cursor = conn.cursor()
+        print('Database connection was successful!')
+        break
+    except Exception as e:
+        print('Database connection was unsuccessful!')
+        print("error", e)
+        time.sleep(2)
 
 my_posts = [{'title': 'Post 1', 'content': 'This is a post', 'id': 1},
             {'title': 'Post 2', 'content': 'This is another post', 'id': 2}]
@@ -20,44 +35,30 @@ class Post(BaseModel):
     rating: Optional[int] = None  # Optional field
 
 
-def find_post(id):
-    print('input', id)
-    for post in my_posts:
-        print(post['id'])
-        if post['id'] == id:
-            return post
-    return None
-
-
-def find_index_post(id):
-    for i, p in enumerate(my_posts):
-        if p['id'] == id:
-            return i
-
-
 @app.get('/posts')  # Decorator
 async def root():  # Name these function as descriptive as possible
-    return {'data': my_posts}
+    cursor.execute("""SELECT * FROM posts""")
+    posts = cursor.fetchall()
+    print(posts)
+    return {'data': posts}
 
 
 @app.post('/createposts', status_code=status.HTTP_201_CREATED)
 def create_post(post: Post):
-    post_dict = post.model_dump()
-    post_dict['id'] = randrange(0, 1000000)
-    my_posts.append(post_dict)
+    cursor.execute("""INSERT INTO posts(title, content,published) VALUES(%s,%s,%s) RETURNING *""",
+                   (post.title, post.content, post.published))
+    new_post = cursor.fetchone()
+    conn.commit()  # commit the changes to db
     return {
-        'data': my_posts
+        'data': new_post
     }
 
 
 @app.get('/posts/{id}')
-def get_post(id: int, response: Response):  # automatically converts to int
-    post = find_post(id)
+def get_post(id: int):  # automatically converts to int
+    cursor.execute("""SELECT * FROM posts WHERE id = %s""", (str(id)))
+    post = cursor.fetchone()
     if post is None:
-        # response.status_code = status.HTTP_404_NOT_FOUND
-        # return {
-        #     'data': 'Post with id ' + str(id) + ' not found'
-        # }
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail='Post with id ' + str(id) + ' not found')
     return {
@@ -67,21 +68,26 @@ def get_post(id: int, response: Response):  # automatically converts to int
 
 @app.delete('/posts/{id}', status_code=status.HTTP_204_NO_CONTENT)
 def delete_post(id: int):
-    index = find_index_post(id)
-    if index is None:
+    cursor.execute(
+        """DELETE FROM posts WHERE id = %s RETURNING *""", (str(id)))
+    deleted_post = cursor.fetchone()
+    conn.commit()
+
+    if deleted_post is None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail='Post with id ' + str(id) + ' not found')
-    my_posts.pop(index)
 
 
 @app.put('/posts/{id}')
 def update_post(id: int, post: Post):
-    index = find_index_post(id)
 
-    if index == None:
+    cursor.execute("""UPDATE posts SET title = %s, content = %s, published = %s WHERE id = %s RETURNING *""",
+                   (post.title, post.content, post.published, str(id)))
+    updated_post = cursor.fetchone()
+    conn.commit()
+
+    if updated_post == None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail=f"post with id :{id} does not exists.")
-    post_dict = post.model_dump()
-    post_dict['id'] = id
-    my_posts[index] = post_dict
-    return {'data': post_dict}
+
+    return {'data': updated_post}
